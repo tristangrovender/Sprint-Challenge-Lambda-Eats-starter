5ac9c7cfb229b7c75357cbb19a00dd10df04894a
Tristan Grovender <therealtristangrovender@gmail.com>
2020-04-10 15:07:13 -0500

    Added some basic styling

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import { Link, Route } from "react-router-dom";
 import Home from "./components/Home.js";
 import Form from "./components/Form.js";
+import "./App.css";
 
 const App = () => {
     return (
-        <>
-            <h1>Lambda Eats</h1>
+        <div className="App">
+            <h1 className="header">Lambda Eats</h1>
             <Link exact to="/">
                 Home
             </Link>
@@ -14,7 +15,7 @@ const App = () => {
             <Link to="/pizza">Order now!</Link>
             <Route path="/" exact component={Home} />
             <Route path="/pizza" component={Form} />
-        </>
+        </div>
     );
 };
 export default App;
